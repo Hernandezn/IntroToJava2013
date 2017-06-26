7108e2523819732ea46f4c1640c82bdb495e384f
Nicolas <hernandezn@live.com>
2017-06-26 02:11:26 -0400

    Delete InsuredCarDemo.java

--- InsuredCarDemo.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class InsuredCarDemo{
-	public static void main(String[]args){
-		InsuredCar myCar=new InsuredCar();
-		JOptionPane.showMessageDialog(null,myCar.toString());
-	}
-}
